3d2b9580a0a664be71104082e4d500ed4b0ad03c
jimt2000 <jimt2000@users.noreply.github.com>
2022-02-16 17:38:18 -0700

    Change case back to SQLCipher

--- Package.swift
+++ Package.swift
@@ -13,14 +13,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "sqlcipher", url: "https://github.com/jimt2000/sqlcipher", from: "0.1.0"),
+        .package(name: "SQLCipher", url: "https://github.com/jimt2000/sqlcipher", from: "0.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "JetDatabaseCipher",
-            dependencies: ["sqlcipher"],
+            dependencies: ["SQLCipher"],
             cSettings: [
                 .define("SQLITE_HAS_CODEC", to: "1"),
                 .define("SQLITE_TEMP_STORE", to: "3"),      // Probably not necessary, but keeping as a reference
@@ -32,7 +32,7 @@ let package = Package(
             ]),                                             // Probably not necessary, ...
         .testTarget(
             name: "JetDatabaseCipherTests",
-            dependencies: ["JetDatabaseCipher", "sqlcipher"],
+            dependencies: ["JetDatabaseCipher", "SQLCipher"],
             resources: [.process("Resources/")]
         )
   ]
